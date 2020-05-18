8ec4f9aff2dd41c39fb7b9b1bb756ac17987fe4c
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2020-05-18 23:25:38 +0200

    Edit dashboard layout

--- src/views/custom/DashboardCustom.vue
+++ src/views/custom/DashboardCustom.vue
@@ -1,24 +1,37 @@
 <template>
   <div>
-    <CRow>
-      <CCol>
-        <GradesDonutComponent></GradesDonutComponent>
-      </CCol>
-      <CCol>
-        <ExpirationDonutComponent></ExpirationDonutComponent>
-      </CCol>
-    </CRow>
     <CCard>
       <CCardBody>
         <CRow>
           <CCol sm="5">
-            <h4 id="traffic" class="card-title mb-0">Grade history</h4>
+            <h4 class="card-title mb-0">Grade history</h4>
           </CCol>
         </CRow>
         <GradeHistoryGraphComponent style="height:300px;margin-top:40px;"/>
       </CCardBody>
-
     </CCard>
+    <CRow>
+      <CCol>
+        <CCard>
+          <CCardHeader>
+            <h4 class="card-title mb-0">Current grades for active targets</h4>
+          </CCardHeader>
+          <CCardBody>
+            <GradesDonutComponent></GradesDonutComponent>
+          </CCardBody>
+        </CCard>
+      </CCol>
+      <CCol>
+        <CCard>
+          <CCardHeader>
+            <h4 class="card-title mb-0">Distribution of expirations for active targets</h4>
+          </CCardHeader>
+          <CCardBody>
+            <ExpirationDonutComponent></ExpirationDonutComponent>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
   </div>
 </template>
 

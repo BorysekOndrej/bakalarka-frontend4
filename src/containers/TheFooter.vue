f7b6ec14b1629fbc9fe38aaae87c8e4373ae6ed4
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2020-07-08 23:38:10 +0200

    icon, sidebar , footer

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -1,12 +1,13 @@
 <template>
   <CFooter :fixed="false">
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
-      <span class="ml-1">&copy; {{new Date().getFullYear()}} creativeLabs.</span>
+      <a href="https://github.com/BorysekOndrej/bakalarka-frontend4" target="_blank">TLS Inventory</a>
     </div>
     <div class="ml-auto">
-      <span class="mr-1">Powered by</span>
-      <a href="https://coreui.io/vue" target="_blank">CoreUI for Vue</a>
+      <span class="mr-1"></span>
+      Template <a href="https://coreui.io/vue" target="_blank">CoreUI for Vue</a>,
+      Icon <a href="https://thenounproject.com/icon/2631109/" target="_blank">Security by Adrien Coquet from the Noun Project</a>
+
     </div>
   </CFooter>
 </template>
